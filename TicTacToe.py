51c94dff552a56e8138af62a9bb7313cc0fee1f8
Myth <muhammad.j1441@gmail.com>
2026-01-07 12:41:10 +0000

    TicTacToe.py
    TicTacToe checkwin

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,37 +1,39 @@
-board = ["X","X","X",
-		"O","","O",
-		"","",""]
+board = [["X","","X"],
+		["O","X","O"],
+		["X","","O"]]
 
 def CheckWin(board):
     # Checking win for rows
-    if board[0] == board[1] == board[2] != "":
-        return board[0]
-	if board[3] == board[4] == board[5] != "":
-        return board[3]
-    if board[6] == board[7] == board[8] != "":
-        return board[6]
+    if board[0][0] == board[0][1] == board[0][2] != "":
+        return board[0][0]
+    if board[1][0] == board[1][1] == board[1][2] != "":
+        return board[1][0]
+    if board[2][0] == board[2][1] == board[2][2] != "":
+        return board[2][0]
 
 
     # Checking win for columns
-    if board[0] == board[3] == board[6] != "":
-        return board[0]
-    if board[1] == board[4] == board[7] != "":
-        return board[1]
-    if board[2] == board[5] == board[8] != "":
-        return board[2]
+    if board[0][0] == board[1][0] == board[2][0] != "":
+        return board[0][0]
+    if board[0][1] == board[1][1] == board[2][1] != "":
+        return board[0][1]
+    if board[0][2] == board[1][2] == board[2][2] != "":
+        return board[0][2]
 
 
     # Checking Diagonal
-    if board[0] == board[4] == board[8] != "":
-        return board[0]
-    if board[2] == board[4] == board[6] != "":
-        return board[2]
+    if board[0][0] == board[1][1] == board[2][2] != "":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != "":
+        return board[0][2]
     return None
 
 
 player = CheckWin(board)
 
-if CheckWin(board):
+if CheckWin(board) == "X":
+    print(f"{player} wins!")
+elif CheckWin(board) == "O":
     print(f"{player} wins!")
 else:
     pass
